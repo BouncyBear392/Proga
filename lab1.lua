1d5bc61f1f7dc03f56eb1ed4ab1f9de5d8a44ad3
BouncyBear392 <50376724+BouncyBear392@users.noreply.github.com>
2019-12-09 12:24:35 +0300

    Create lab1.lua

---
+++ lab1.lua
@@ -0,0 +1,40 @@
+--- ПРОГРАММА НА 3 ---
+print("Введите два числа")
+a = io.read("*n")
+b = io.read("*n")
+sum = a + b
+print("Ваша сумма = ",sum)
+
+
+--- ПРОГРАММА НА 4 ---
+print("Введите два числа")
+a = io.read("*n")
+b = io.read("*n")
+sum = a + b
+min = a - b
+umn = a*b
+del = a/b
+print("Ваша сумма = ",sum)
+print("Ваша разность = ",min)
+print("Ваше произведение = ",umn)
+print("Ваше деление = ",del)
+
+
+--- ПРОГРАММА НА 5 ---
+
+
+print("Введите число")
+a = io.read("*n")
+
+b = math.pi
+c = (a*b)/180
+sin = math.sin(c)
+cos = math.cos(c)
+
+print("Ваш косинус = ",cos)
+print("Ваш синус = ",sin)
+
+
+
+
+
